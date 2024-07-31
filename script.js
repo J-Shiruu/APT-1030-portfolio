6af9899433e07b4a9ef31382fd5bbdfdba7656b8
J-Shiruu <wjanji576@gmail.com>
2024-07-31 19:01:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).on("scroll", function(){
+    if($(document).scrollTop() > 86)
+    {
+      $("#navbar").addClass("sticky");
+      $("#nav-logo").addClass("color-white");
+      $(".nav-custom-link").addClass("color-white");
+      $(".navbar-btn").addClass("nav-btn-icon");
+    }
+    else
+    {
+        $("#navbar").removeClass("sticky");
+        $("#nav-logo").removeClass("color-white");
+        $(".nav-custom-link").removeClass("color-white");
+        $(".navbar-btn").removeClass("nav-btn-icon");
+    }
+
+
+    // Back to top button
+    const backToTopButton = document.querySelector('.back-to-top');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopButton.classList.add('show');
+        } else {
+            backToTopButton.classList.remove('show');
+        }
+    });
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
